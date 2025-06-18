a096c3c66e0c0ec55a5a756dd8f8f771ca893fc5
Salami Azeez <salamitopeazeez@gmail.com>
2025-06-18 17:58:47 +0100

    Added plus sign on contact section

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,7 +1,38 @@
-
+import { section } from "motion/react-client";
+import { FlipLink } from "./RevealLinks";
 
 export const Contact = () => {
   return (
-    <div>contact</div>
-  )
-}
+    <section className="relative h-screen bg-white text-black p-20">
+        {/* top left */}
+      <div className="absolute top-[50px] left-[50px]">
+        <div className="relative">
+          <span className="block w-1 h-10 bg-black"></span>
+          <span className="absolute top-0 block w-1 h-10 bg-black rotate-90"></span>
+        </div>
+      </div>
+      {/* top right */}
+      <div className="absolute top-[50px] right-[50px]">
+        <div className="relative">
+          <span className="block w-1 h-10 bg-black"></span>
+          <span className="absolute top-0 block w-1 h-10 bg-black rotate-90"></span>
+        </div>
+      </div>
+      {/* bottom left  */}
+      <div className="absolute bottom-[50px] left-[50px]">
+        <div className="relative">
+          <span className="block w-1 h-10 bg-black"></span>
+          <span className="absolute top-0 block w-1 h-10 bg-black rotate-90"></span>
+        </div>
+      </div>
+      {/* bottom right */}
+      <div className="absolute bottom-[50px] right-[50px]">
+        <div className="relative">
+          <span className="block w-1 h-10 bg-black"></span>
+          <span className="absolute top-0 block w-1 h-10 bg-black rotate-90"></span>
+        </div>
+      </div>
+      
+    </section>
+  );
+};
