8ba57e2ecd4b5f57d40d69dc585231804b46d037
Salami Azeez <salamitopeazeez@gmail.com>
2025-06-28 00:56:04 +0100

    Added useLenis and initLenis

---
+++ src/lib/lenis.ts
@@ -0,0 +1,41 @@
+import Lenis from 'lenis';
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+
+export const useLenis = () => {
+  useGSAP(() => {
+    const lenis = new Lenis({
+      lerp: 0.1,
+    });
+
+    lenis.on('scroll', () => {
+      ScrollTrigger.update();
+    });
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+    };
+  });
+};
+
+export const initLenis = () => {
+  const lenis = new Lenis({
+    lerp: 0.1,
+  });
+
+  lenis.on('scroll', ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+
+  return lenis;
+};
