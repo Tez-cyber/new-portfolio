de61b43fb299d48f8311dee857c1271a32819f21
Salami Azeez <salamitopeazeez@gmail.com>
2025-06-28 00:58:53 +0100

    Added initLenis hook in portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,11 +2,12 @@
 import { PortfolioHeroSection } from "@/components/portfolio/portfolioHeroSection";
 import { PortfolioOutroSection } from "@/components/portfolio/portfolioOutroSection";
 import { PortfolioStickySection } from "@/components/portfolio/portfolioStickySection";
+import { initLenis } from "@/lib/lenis";
 import { useGSAP } from "@gsap/react";
 
 export default function Portfolio() {
 
-  useGSAP(() => {});
+  useGSAP(() => { initLenis() });
   return (
     <section>
       <PortfolioHeroSection />
