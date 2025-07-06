6943fd0d5042b7c87a3d2ec1518762e76861243a
Salami Azeez <salamitopeazeez@gmail.com>
2025-07-06 19:58:53 +0100

    Made animation section hidden on mobile

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,7 +19,7 @@ const switzer = localFont({
     },
   ],
   variable: "--font-blackItalic",
-  display: "swap"
+  display: "swap",
 });
 export default function About() {
   const overlayRef = useRef<HTMLDivElement>(null);
@@ -68,7 +68,7 @@ export default function About() {
         ease: "power2.inOut",
         scrollTo: {
           y: nextSectionRef.current,
-          offsetY: 80, 
+          offsetY: 80,
           autoKill: false,
         },
         onStart: () => {
@@ -80,7 +80,7 @@ export default function About() {
           }
         },
       });
-    }, 50); 
+    }, 50);
   }, []);
 
   useGSAP(
@@ -130,7 +130,7 @@ export default function About() {
           gsap.to(scrollIndicatorRef.current, {
             opacity: 0,
             y: 20,
-            duration: 0.3,
+            duration: 0.1,
           });
         },
       });
@@ -217,64 +217,68 @@ export default function About() {
   };
   return (
     <>
-    <Navbar />
-    <section
-      className="relative w-screen"
-    >
-      <section className="">
-        {/* Background content */}
-        <section className="h-screen relative flex flex-col items-center justify-center p-[3em] bg-white text-black">
-          <h1 style={textStyles} className={`${textDisplay} italic`}>
-            about <br />
-            me
-          </h1>
-          <span
-            ref={buttonRef}
-            className="hover-btn absolute bottom-[80px] left-[80px] w-[60px] h-[60px] rounded-full bg-black flex cursor-pointer mt-8"
-          >
-            <ArrowIcon color="white" />
-          </span>
-        </section>
+      <Navbar />
+      <section className="relative w-screen hidden md:block">
+        <section className="">
+          {/* Background content */}
+          <section className="h-screen relative flex flex-col items-center justify-center p-[3em] bg-white text-black">
+            <h1 style={textStyles} className={`${textDisplay} italic`}>
+              about <br />
+              me
+            </h1>
+            <span
+              ref={buttonRef}
+              className="hover-btn absolute bottom-[80px] left-[80px] w-[60px] h-[60px] rounded-full bg-black flex cursor-pointer mt-8"
+            >
+              <ArrowIcon color="white" />
+            </span>
+          </section>
 
-        {/* Overlay section */}
-        <section
-          ref={overlayRef}
-          style={{
-            clipPath: "circle(50px at var(--x, 50%) var(--y, 50%))",
-            willChange: "clip-path, backdrop-filter",
-          }}
-          className="h-screen flex flex-col items-center justify-center p-[3em] bg-black text-white absolute top-0 left-0 w-full backdrop-blur-sm"
-        >
-          <h1 style={textStyles} className={`${textDisplay} italic`}>
-            about <br />
-            me
-          </h1>
-          <span
-            ref={toggleButtonRef}
-            onClick={handleToggleClick}
-            className="hover-btn2 absolute bottom-[80px] left-[80px] w-[60px] h-[60px] rounded-full bg-white flex cursor-pointer mt-8"
+          {/* Overlay section */}
+          <section
+            ref={overlayRef}
+            style={{
+              clipPath: "circle(50px at var(--x, 50%) var(--y, 50%))",
+              willChange: "clip-path, backdrop-filter",
+            }}
+            className="h-screen flex flex-col items-center justify-center p-[3em] bg-black text-white absolute top-0 left-0 w-full backdrop-blur-sm"
           >
-            <ArrowIcon color="black" />
-          </span>
+            <h1 style={textStyles} className={`${textDisplay} italic`}>
+              about <br />
+              me
+            </h1>
+            <span
+              ref={toggleButtonRef}
+              onClick={handleToggleClick}
+              className="hover-btn2 absolute bottom-[80px] left-[80px] w-[60px] h-[60px] rounded-full bg-white flex cursor-pointer mt-8"
+            >
+              <ArrowIcon color="black" />
+            </span>
 
-          {/* Scroll down indicator */}
-          <div
-            ref={scrollIndicatorRef}
-            onClick={handleScrollDown}
-            className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center text-white opacity-0 cursor-pointer hover:opacity-80 transition-opacity"
-          >
-            <span className="text-sm mb-2">SCROLL DOWN</span>
-            <ArrowDownIcon />
-          </div>
+            {/* Scroll down indicator */}
+            <div
+              ref={scrollIndicatorRef}
+              onClick={handleScrollDown}
+              className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center text-white opacity-0 cursor-pointer hover:opacity-80 transition-opacity"
+            >
+              <span className="text-sm mb-2">SCROLL DOWN</span>
+              <ArrowDownIcon />
+            </div>
+          </section>
         </section>
       </section>
-    </section>
-    {display && (
-        <>
+      {display && (
+        <section className="hidden md:block">
           <NextSection ref={nextSectionRef} />
           <Contact />
-        </>
+        </section>
       )}
+      
+      {/* Mobile */}
+      <section className="md:hidden">
+        <NextSection ref={nextSectionRef} />
+        <Contact />
+      </section>
     </>
   );
 }
