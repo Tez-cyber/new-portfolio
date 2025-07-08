ffa9a17c30433d17d619935a4bb011f83a89c7f5
Salami Azeez <salamitopeazeez@gmail.com>
2025-07-08 17:04:02 +0100

    Completed Intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,15 @@ export default function Home() {
           {/* Parent grid for top section */}
           <div className="rounded-xl lg:h-[205px] grid gap-4 lg:grid-cols-[48%_24%_24%]">
             {/* First */}
-            <div className="bg-[#191919] p-20 rounded-2xl">
-              Intro
+            <div className="bg-[#191919] rounded-2xl p-10">
+              <div className="flex gap-3 items-center text-3xl">
+                <span>Hello I'm Azeez </span>
+                <span className="w-5 h-[2px] block bg-textPrimary" />
+              </div>
+              <span className="text-[#676767] text-lg text pt-5 block">
+                Lead developer at <a className="underline transition-all duration-300 ease-in-out hover:text-white" target="_blank" href="https://ns-express.netlify.app/">Nsexpress</a>, <br />
+                based in Nigeria
+              </span>
             </div>
             {/* Second */}
             {/* Display for md devices */}
